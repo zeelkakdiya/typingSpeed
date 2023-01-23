887fb8508b858875bed22442ef49ff561bdf5430
zeel <zeelkakdiya2002@gmail.com>
2023-01-23 21:58:05 +0530

    typing speed

---
+++ src/components/Results.tsx
@@ -0,0 +1,74 @@
+import { motion } from 'framer-motion';
+import { State } from '../hooks/useEngine';
+import { formatPercentage } from '../utils/helpers';
+
+const Results = ({
+  state,
+  errors,
+  accuracyPercentage,
+  total,
+  className = '',
+  WPM,
+}: {
+  state: State;
+  errors: number;
+  accuracyPercentage: number;
+  total: number;
+  className?: string;
+  WPM?: number;
+}) => {
+  if (state !== 'finish') {
+    return null;
+  }
+
+  const initial = { opacity: 0 };
+  const animate = { opacity: 1 };
+
+  return (
+    <motion.ul
+      initial={initial}
+      animate={animate}
+      className={`flex flex-col items-center text-green-500 space-y-3 ${className}`}
+    >
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ duration: 0.3 }}
+        className="text-xl font-semibold"
+      >
+        Results
+      </motion.li>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ duration: 0.3, delay: 0.5 }}
+      >
+        Accuracy: {formatPercentage(accuracyPercentage)}
+      </motion.li>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ duration: 0.3, delay: 1 }}
+        className="text-red-500"
+      >
+        Errors: {errors}
+      </motion.li>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ duration: 0.3, delay: 1.4 }}
+      >
+        Typed: {total}
+      </motion.li>
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ duration: 0.3, delay: 1.4 }}
+      >
+        WPM: {WPM}
+      </motion.li>
+    </motion.ul>
+  );
+};
+
+export default Results;
